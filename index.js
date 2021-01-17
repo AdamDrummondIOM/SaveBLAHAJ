9d9aa4555e35b1e86ea6180d71afcd271888b030
Adam Drummond <adam.drummond9@gmail.com>
2021-01-17 16:00:12 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+require("express");
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: '### FIREBASE API KEY ###',
+    authDomain: '### FIREBASE AUTH DOMAIN ###',
+    projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+  });
+  
+  var db = firebase.firestore();
+  var app = express();
+  var path = require('path');
+
+  app.get('/', function(req, res){
+    resi.sendFile(path.join(__dirname +  '/index.html'))
+  });
+
+function post(postText){
+    db.collection("posts").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+}
+
